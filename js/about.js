066c58307f0f4b0b04c02a84f01642abb7179316
Nurmuhammad <nurmuhammadrahimov338@gmail.com>
2025-02-13 16:37:22 +0500

    test

--- js/about.js
+++ js/about.js
@@ -4,14 +4,3 @@ function toggleContent(element) {
     content.classList.toggle('active');
 }
 
-// Desktop hover effect
-if (window.innerWidth >= 1024) {
-    document.querySelectorAll('.ios-list-item').forEach(item => {
-        item.addEventListener('mouseenter', () => {
-            item.style.backgroundColor = '#F8F8F8';
-        });
-        item.addEventListener('mouseleave', () => {
-            item.style.backgroundColor = 'white';
-        });
-    });
-}
